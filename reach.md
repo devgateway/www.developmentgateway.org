f46360ee9e61f53dee0c03bd998be24955e51308
phil <uniphil@gmail.com>
2015-03-06 00:37:04 -0500

    add rough Reach page

--- reach.md
+++ reach.md
@@ -3,8 +3,30 @@ layout: page
 title: Reach
 permalink: /reach/
 weight: 2
+hide_header: true
+extrastyles: [ '/lib/leaflet/leaflet.css' ]
+extrascripts: [ '/lib/leaflet/leaflet.js', '/scripts/reach-map.js' ]
 ---
 
-Donec vestibulum nunc nisi, vulputate porta felis facilisis a. Nam tempor elit a nisi lobortis, eu venenatis tellus dapibus. Nam vel imperdiet enim. In sed pretium diam, sit amet eleifend nibh. Donec lacus arcu, venenatis nec lacus cursus, aliquam eleifend mi. Praesent est lacus, lobortis in rhoncus sit amet, suscipit vel neque. Ut vehicula tempor libero vitae sagittis. Vestibulum congue risus a tristique rhoncus. Duis posuere lacus nec tempus sodales. Vivamus eget malesuada lorem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur mattis nec erat et porttitor. Praesent scelerisque massa quis ex vulputate, ut ultrices sapien venenatis. Proin sem tellus, pretium quis auctor ut, finibus nec nibh. Aenean gravida laoreet mollis.
+{% include map.html countries=site.data.reach_countries %}
 
-Quisque sed justo ut purus cursus imperdiet id sed sapien. Praesent nec tincidunt velit, at tempus risus. Aenean malesuada pellentesque libero, lobortis congue enim sodales ut. In vestibulum euismod iaculis. Vivamus vel aliquet eros, sit amet sollicitudin purus. Sed at quam nibh. Nulla pellentesque metus vitae ex congue ultrices. Nullam eu lacinia felis, et semper turpis. Phasellus id blandit purus. Etiam commodo sed lectus eu consectetur. Proin quis felis vel diam varius convallis eget et dolor. Sed vitae sagittis mi, vitae suscipit nibh. Curabitur elementum nulla justo, quis molestie ligula posuere non. Cras sit amet ex fringilla, pulvinar felis a, molestie massa. Donec augue ante, scelerisque sed tortor nec, lacinia vulputate neque. Aliquam erat volutpat.
+
+{% for country in site.data.reach_countries %}
+
+<h2 id="{{ country.isoA2 }}" name="{{ country.isoA2 }}">
+  <img href="/images/flags/{{ country.isoA2 }}.png" alt="Flag for {{ country.name }}" />
+  {{ country.name }} &ndash; {{ country.year }}
+</h2>
+
+
+{{ country.summary }}
+
+{% if country.pullQuote %}
+> {{ country.pullQuote }}
+{% if country.pullSource %}
+>
+> &mdash;{{ country.pullSource }}
+{% endif %}
+{% endif %}
+
+{% endfor %}
---
+++ scripts/reach-map.js
@@ -0,0 +1,139 @@
+(function(mapEl) {
+  if (!mapEl) {
+    throw new Error('no map element ("#reach-map") found');
+  }
+
+  function sizeMapContainer(mapEl) {
+    // expand to the viewport size
+    var containedWidth = mapEl.offsetWidth;
+    var offsetTop = (function getOffset(node, offset) {
+      return node ?
+        getOffset(node.offsetParent, (offset || 0) + node.offsetTop) :
+        offset;
+    })(mapEl);
+    var viewW = window.innerWidth || document.documentElement.clientWidth;
+    var viewH = Math.max(document.documentElement.clientHeight, window.innerHeight || 200);
+    mapEl.style.width = viewW + 'px';
+    mapEl.style.height = (viewH - offsetTop) + 'px';
+    mapEl.style.marginLeft = -Math.round((viewW - containedWidth) / 2) + 'px';
+  }
+
+  function initMap(mapEl) {
+    if (!(mapEl.getAttribute('data-tiles'))) {
+      throw new Error('#reach-map must have a "data-tiles" property.');
+    }
+    // set up the map
+    var map = L.map(mapEl, {
+      scrollWheelZoom: false,
+      zoomControl: false,  // re-added later in the desired location
+    }).setView([0,0],1);
+    var needToZoomBy = map.getBounds()._northEast.lng / (180*1.5);
+    if (needToZoomBy > 1) {
+      var zoomSteps = Math.ceil(Math.log(needToZoomBy) / Math.log(2));
+      map.zoomIn(zoomSteps);
+    }
+    L.tileLayer(mapEl.getAttribute('data-tiles'), {
+      attribution: 'Tiles <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> <a href="http://stamen.com">Stamen Design</a> | Data <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a> <a href="http://openstreetmap.org">OpenStreetMap</a>',
+      worldCopyJump: true,
+      minZoom: parseInt(mapEl.getAttribute('data-min-zoom') || 0, 10),
+      maxZoom: parseInt(mapEl.getAttribute('data-max-zoom') || 5, 10)
+    }).addTo(map);
+    L.control.zoom({
+      position: 'bottomright',
+      zoomOutText: '',  // we will draw our own centred minus with css
+    }).addTo(map);
+    return map;
+  }
+
+  function initInset(mapEl) {
+    var h = mapEl.querySelector('.map-inset-text > h1'),
+        p = mapEl.querySelector('.map-inset-text > p.help'),
+        orinalText = {
+          h: h.textContent,
+          p: p.textContent
+        };
+    function rewrite(to) {
+      h.textContent = to.h;
+      p.textContent = to.p;
+    }
+    return {
+      detail: function(data) {
+        return function() {
+          rewrite({h: data.name, p: data.year});
+        }
+      },
+      reset: function(data) {
+        rewrite(orinalText);
+      }
+    }
+  }
+
+  function parseDomData(qs) {
+    var dataEls = document.querySelectorAll(qs),
+        i,
+        data = [],
+        convert = {
+          string: function(v) { return v; },
+          float: function(v) { return parseFloat(v); },
+          int: function(v) { return parseInt(v, 10); }
+        };
+
+    function pickData(el, picks) {
+      var d = {};
+      for (var k in picks) {
+        if (picks.hasOwnProperty(k)) {
+          d[k] = picks[k](el.getAttribute('data-' + k));
+        }
+      }
+      return d;
+    }
+
+    for (i=0; i<dataEls.length; i++) {
+      data.push(pickData(dataEls[i], {
+        name: convert.string,
+        isoA2: convert.string,
+        lat: convert.float,
+        lon: convert.float,
+        year: convert.int,
+        program: convert.string,
+        summary: convert.string,
+        pullQuote: convert.string,
+        pullSource: convert.string
+      }));
+    }
+    return data;
+  }
+
+  function drawMarkers(map, data, inset) {
+    var marker;
+
+    function locate(where) {
+      return function() {
+        window.location.hash = where;
+      }
+    }
+
+    for (var i=0; i<data.length; i++) {
+      marker = L.marker([data[i].lat, data[i].lon], {
+        icon: L.divIcon({
+          className: 'country-marker',
+          iconSize: null  // don't let leaflet put a size on the el style  >:(
+        })
+      });
+      marker.on('click', locate(data[i].isoA2));
+      marker.on('mouseover', inset.detail(data[i]));
+      marker.on('mouseout', inset.reset);
+      marker.addTo(map);
+    }
+  }
+
+  // ready set go
+  (function draw(mapEl) {
+    sizeMapContainer(mapEl);
+    var inset = initInset(mapEl);
+    var map = initMap(mapEl);
+    var data = parseDomData('.reach-data > .reach-map-data');
+    drawMarkers(map, data, inset);
+  })(mapEl);
+
+})(document.getElementById('reach-map'));
