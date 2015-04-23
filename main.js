eecd6f029977b39369fb7c673a6e968fbd0b9d45
Schneider Werner-Walter <wschneider@developmentgateway.org>
2015-04-23 17:40:07 +0300

    Use the "nanoajax" library to display content on the expertise page.

---
+++ main.js
@@ -0,0 +1,11 @@
+---
+---
+
+{% include_relative lib/classList/classList.min.js %}
+{% include_relative lib/modernizr/modernizr-custom.min.js %}
+{% include_relative lib/nanoajax/nanoajax.min.js %}
+{% include_relative lib/jRespond/js/jRespond.min.js %}
+{% include_relative lib/svg4everybody/svg4everybody.min.js %}
+
+{% include_relative lib/requestAnimationFrame/polyfill.min.js %}
+{% include_relative lib/smooth-scroll/smooth-scroll.min.js %}
